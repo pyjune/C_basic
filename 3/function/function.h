596420648e6a2c1d4eaa18d7e2b45d7338579b5f
pyjune <pyjune@naver.com>
2017-11-01 15:45:36 +0900

    Create function.h

---
+++ 3/function/function.h
@@ -0,0 +1,14 @@
+#ifndef __FUNCTION_H__
+#define __FUNCTION_H__
+
+// 인자도 없고 리턴값도 없는 경우..
+void f1(void);
+// 인자가 있고 리턴값은 없는 경우..
+void f2(int n);
+// 인자는 없고 리턴값은 있는 경우..
+int f3(void);
+// 인자도 있고 리턴값도 있는 경우..
+int f4(int n); 
+
+
+#endif // __FUNCTION_H__
