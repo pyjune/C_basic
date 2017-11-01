14ab3739eef774c2baff9dd864e3d87ddce8a4e6
pyjune <pyjune@naver.com>
2017-11-01 16:40:27 +0900

    Update main.cpp

--- 3/function/main.cpp
+++ 3/function/main.cpp
@@ -1,10 +1,21 @@
 #include <stdio.h>
 #include "function.h"
 
+int gVar1;		// 함수끼리 혹은 파일끼리 공유하는 경우 
+int gVar2 = 10;
+int gVar3;
+int gArr[1000000]; // 크기가 큰 경우 전역에.. 
+void change_gVar1(int n);
 
 int main(void) 
 {
 	int n = 100;
+	
+	printf("gVar1 = %d\n", gVar1); 
+	printf("gVar2 = %d\n", gVar2);
+	change_gVar1(100);
+	printf("gVar1 = %d\n", gVar1);
+	
 	f1();
 	f2(100);
 	// f2(n);
@@ -13,6 +24,13 @@ int main(void)
 	// printf("r = %d\n", f3());
 	r = f4(3);
 	printf("r = %d\n", r);
+	
+	
 	return 0;
+}
+
+void change_gVar1(int n)
+{
+	gVar1 = n;
 }
 
