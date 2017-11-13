ebef4ddd39f958f6278e7b9ca3f9984e415cafda
pyjune <pyjune@naver.com>
2017-11-13 14:40:39 +0900

    Update main.cpp

--- 6/score/main.cpp
+++ 6/score/main.cpp
@@ -2,7 +2,9 @@
 #include "score.h"
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
-
+void find_1st(int N, ScoreType score[]);
+void find_last(int N, ScoreType score[]);
+void print_sort(int N, ScoreType score[]);
 
 int main(int argc, char** argv) {
 	
@@ -13,9 +15,50 @@ int main(int argc, char** argv) {
 	
 	int N = 0; // 학생수 
 	scanf("%d", &N);
+	// readdata();
+	// calc();
 	
 	print_all(N, score);
+	find_1st(N, score); // 평균점수가 print_all()에서 계산이 되므로.. 
+	find_last(N, score);
+	print_sort(N, score);
 	
 	return 0;
 }
 
+void print_sort(int N, ScoreType score[])
+{
+	
+}
+
+void find_1st(int N, ScoreType score[])
+{
+	double maxAvg = 0;
+	// double maxAvg = score[0].avg;
+	
+	int maxIdx = 0; // 값이 아니라 누구인지를 찾으려고 함.. 
+	for(int i = 1; i<N; i++) // i=0인 경우 자신과 비교하면서 시작 
+	{
+		if(score[maxIdx].avg<score[i].avg)
+		{
+			maxIdx = i;
+		 } 
+	}
+	printf("1등 : %d %s %.2f점\n", score[maxIdx].num, score[maxIdx].name, 
+								score[maxIdx].avg);
+}
+
+void find_last(int N, ScoreType score[])
+{
+	
+	int minIdx = 0; // 값이 아니라 누구인지를 찾으려고 함.. 
+	for(int i = 1; i<N; i++) // i=0인 경우 자신과 비교하면서 시작 
+	{
+		if(score[minIdx].avg > score[i].avg)
+		{
+			minIdx = i;
+		 } 
+	}
+	printf("%d등 : %d %s %.2f점\n", N,score[minIdx].num, score[minIdx].name, 
+								score[minIdx].avg);
+}
